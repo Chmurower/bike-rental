2a64fd0012c49e606351ab898038ae07164157e2
lzlocki <lzlocki@poczta.fm>
2023-10-08 21:23:52 +0200

    Add db context

---
+++ bike-rental.backend/BikeRental.Models/RentalDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BikeRental.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeRental.Models
+{
+    public class RentalDbContext : DbContext
+    {
+       public RentalDbContext(DbContextOptions<RentalDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Bicycle> Bicycles { get; set; }
+        public DbSet<Category> Categorys { get; set; } 
+
+
+
+    }
+}
