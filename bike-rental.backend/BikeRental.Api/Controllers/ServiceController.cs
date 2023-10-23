1f1c32e962e2cf812bde965608d05be6204eb455
lzlocki <lzlocki@poczta.fm>
2023-10-23 21:29:28 +0200

    Add controller for service management

---
+++ bike-rental.backend/BikeRental.Api/Controllers/ServiceController.cs
@@ -0,0 +1,19 @@
+﻿using BikeRental.Api.MockDb;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeRental.Api.Controllers
+{
+    [ApiController]
+    public class ServiceController : Controller
+    {
+        private readonly MockDatabase _dbMock = new MockDatabase();
+
+        // GET
+        [HttpGet("api/service")]
+        public IActionResult GetAllBikesInService()
+        {
+            var service = _dbMock.bicycles.Where(i => i.IsInService == true).ToList();
+            return Ok(service);
+        }
+    }
+}
