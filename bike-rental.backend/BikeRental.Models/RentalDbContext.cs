f0601636951ad0798cc3aa37e17314a08a5b4c26
lzlocki <lzlocki@poczta.fm>
2023-10-29 16:29:12 +0100

    Implements direct communication with MySql database server

--- bike-rental.backend/BikeRental.Models/RentalDbContext.cs
+++ bike-rental.backend/BikeRental.Models/RentalDbContext.cs
@@ -5,14 +5,20 @@ namespace BikeRental.Models
 {
     public class RentalDbContext : DbContext
     {
-       public RentalDbContext(DbContextOptions<RentalDbContext> options) : base(options)
+
+        public RentalDbContext()
+        {
+            
+        }
+
+        public RentalDbContext(DbContextOptions<RentalDbContext> options) : base(options)
         {
         }
         
         public DbSet<Bicycle> Bicycles { get; set; }
         public DbSet<Category> Categorys { get; set; } 
         public DbSet<FrameSize> FrameSizes { get; set; }
-        public DbSet<ReservationDate> Reservations { get; set; }
+        public DbSet<ReservationDate> ReservationDates { get; set; }
         public DbSet<ReservationTicket> ReservationTickets { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
