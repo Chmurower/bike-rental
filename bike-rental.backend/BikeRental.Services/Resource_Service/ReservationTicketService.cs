c836bf28ffe4f75ec7229d995a635c4c68b6cd33
lzlocki <lzlocki@poczta.fm>
2023-10-30 22:17:40 +0100

    Add reservation ticket service

---
+++ bike-rental.backend/BikeRental.Services/Resource_Service/ReservationTicketService.cs
@@ -0,0 +1,21 @@
+﻿using BikeRental.Models;
+using BikeRental.Models.Models;
+
+namespace BikeRental.Services.Resource_Service
+{
+    public class ReservationTicketService : IReservationTicketService
+    {
+        private readonly RentalDbContext _db;
+        public List<ReservationTicket> GetAllReservations()
+        {
+            var service = _db.ReservationTickets.ToList();
+            return service;
+        }
+
+        public ReservationTicket GetReservationById(int id)
+        {
+            var service = _db.ReservationTickets.FirstOrDefault(x => x.Id == id);
+            return service;
+        }
+    }
+}
