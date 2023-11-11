2550cefd4e6d34e43555076917b1deede00b24d9
lzlocki <lzlocki@poczta.fm>
2023-11-11 10:53:43 +0100

    Add frame size reference in bicycle model

--- bike-rental.backend/BikeRental.Models/Models/Bicycle.cs
+++ bike-rental.backend/BikeRental.Models/Models/Bicycle.cs
@@ -7,6 +7,7 @@ namespace BikeRental.Models.Models
         public Category? Category { get; set; }
         public string? ModelName { get; set; }
         public int FrameSizeId { get; set; }
+        public FrameSize? FrameSize { get; set; }
         public string? SerialNumber { get; set; }
         public DateTime StartService { get; set; }
         public bool IsAvailable { get; set; }
